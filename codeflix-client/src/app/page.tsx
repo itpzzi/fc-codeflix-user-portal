7c6b2c354c2e8c61e57b82bc5904ebcea7924e40
Ítalo Polazzi <itpzzi@outlook.com>
2025-06-02 17:01:56 -0300

    fix(route): setting "/home" same as "/"

--- codeflix-client/src/app/page.tsx
+++ codeflix-client/src/app/page.tsx
@@ -1,9 +1,1 @@
-export default function Home() {
-  return (
-    <div className="relative h-full lg:h-[140vh]">
-      <main className="relative space-y-24 px-16 pb-20">
-        <h1 className="text-2xl md:text-4xl lg:text-7xl">The Witcher</h1>
-      </main>
-    </div>
-  );
-}
+export { default } from './home/page'
