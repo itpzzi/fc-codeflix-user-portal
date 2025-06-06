f4840ecb5afcb79d54413d7bb03ed160d71133b1
Ítalo Polazzi <itpzzi@outlook.com>
2025-06-06 18:57:27 -0300

    feat(movie-service): use JsonServerApiClient to handle queries

---
+++ codeflix-client/src/app/services/MovieService.ts
@@ -0,0 +1,29 @@
+import { Movie } from "@/types/movie";
+import { createJsonServerClient, JsonServerPaginatedResponse, JsonServerQueryParams } from "@/services/JsonServerApiClient";
+
+const client = createJsonServerClient({
+    baseURL: process.env.BASE_URL || 'http://localhost:3333',
+    defaultHeaders: {},
+    timeout: 5000
+});
+
+export async function getMovies(params?: JsonServerQueryParams): Promise<JsonServerPaginatedResponse<Movie[]>> {
+    return client.getList<Movie>('/movies', { params });
+}
+
+export async function getMovieById(id: string): Promise<Movie> {
+    const response = await client.getItem<Movie>(`/movies/${id}`);
+    return response.data;
+}
+
+export async function getFeaturedMovies(params?: JsonServerQueryParams): Promise<JsonServerPaginatedResponse<Movie[]>> {
+    return client.getList<Movie>('/featured', { params });
+}
+
+export async function searchMovie(title: string, params?: JsonServerQueryParams): Promise<JsonServerPaginatedResponse<Movie[]>> {
+    return getMovies({ ...params, "title_like": title });
+}
+
+export async function getMoviesByGenre(genre: string, params?: JsonServerQueryParams): Promise<JsonServerPaginatedResponse<Movie[]>> {
+    return getMovies({ ...params, "genres_like": genre });
+}
